40675507c78b261bbc0eb5abef825fef6454928d
vsirineni2 <sirineni.vamsi@gmail.com>
2022-07-28 12:45:54 -0400

    second commit

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import requests
 
 def get_quote():
     response = requests.get("https://api.kanye.rest/")
+    response.raise_for_status()
     data = response.json()
     quote = data["quote"]
     canvas.itemconfig(quote_text, text=quote)
